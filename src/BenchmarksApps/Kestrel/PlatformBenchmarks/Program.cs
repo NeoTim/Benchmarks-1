6064424c64ecc2ab410f9dccc5b7dbb16dd772bc
Sebastien Ros <sebastienros@gmail.com>
2020-10-05 16:29:26 -0700

    Reverted changes

--- src/BenchmarksApps/Kestrel/PlatformBenchmarks/Program.cs
+++ src/BenchmarksApps/Kestrel/PlatformBenchmarks/Program.cs
@@ -2,6 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
